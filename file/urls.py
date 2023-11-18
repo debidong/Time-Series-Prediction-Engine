1e578986c0b9684559075c4bf1d51f6b7100dfc5
debidong <bowenyang1217@gmail.com>
2023-11-18 17:38:50 +0800

    feat

--- file/urls.py
+++ file/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('file', GetFileView.as_view()),
     path('upload', FileView.as_view()),
-    path('insert', FileView.as_view()),
+    path('insert', InsertView.as_view()),
     path('delete', FileView.as_view())
 ]
