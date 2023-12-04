ba3f1574e7d615083d63dd77f9f2d7ed7d9e9ac6
debidong <ybw88896519@gmail.com>
2023-12-04 19:30:29 +0800

    feat: heartbeat for ws

--- analysis/consumers.py
+++ analysis/consumers.py
@@ -1,6 +1,6 @@
-# consumers.py
-from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class TrainingProgressConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -9,7 +9,12 @@ class TrainingProgressConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
+        # 启动心跳包定时器
+        self.heartbeat_task = asyncio.ensure_future(self.send_heartbeat())
+
     async def disconnect(self, close_code):
+        # 关闭连接时取消定时器
+        self.heartbeat_task.cancel()
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def send_training_progress(self, event):
@@ -20,3 +25,9 @@ class TrainingProgressConsumer(AsyncWebsocketConsumer):
             'progress': progress,
             'algoID': algoID
         }))
+
+    async def send_heartbeat(self):
+        while True:
+            # 发送心跳包到 WebSocket 客户端
+            await self.send(text_data=json.dumps({'heartbeat': 'ping'}))
+            await asyncio.sleep(10)
