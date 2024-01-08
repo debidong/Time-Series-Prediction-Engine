acdcf862c1075dc9546a2d6fd9e5cf288ce9ad40
debidong <bowenyang1217@gmail.com>
2024-01-08 16:58:26 +0800

    fix: remove heartbeat task

--- analysis/consumers.py
+++ analysis/consumers.py
@@ -15,7 +15,7 @@ class TrainingProgressConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         # 关闭连接时取消定时器
-        self.heartbeat_task.cancel()
+        # self.heartbeat_task.cancel()
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def send_training_progress(self, event):
