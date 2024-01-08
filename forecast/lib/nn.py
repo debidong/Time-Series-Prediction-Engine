1e19da48cb7b9c92d91f02e0d6459a2ead7b8fb4
debidong <bowenyang1217@gmail.com>
2024-01-08 17:58:45 +0800

    fix: dbg for pg3-nn

--- forecast/lib/nn.py
+++ forecast/lib/nn.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import torch
 from sklearn.preprocessing import MinMaxScaler
-
 from analysis.models import Algorithm, Result
 from forecast.models import File
 
@@ -25,26 +24,18 @@ def infer(pk_algo: int, pk_file: int):
     # 数据标准化
     scaler_features = MinMaxScaler()
     scaler_goal = MinMaxScaler()
-
-    features_normalized = scaler_features.fit_transform(training_features.reshape(-1, 1))
-    goal_normalized = scaler_goal.fit_transform(training_goal.reshape(-1, 1))
+    # scaler_goal.fit(training_goal.reshape(-1, 1))
+    training_features_normalized = scaler_features.fit_transform(training_features)
+    input_dim = training_features_normalized.shape[1]
 
     # 准备输入序列
-    input_dim = features_normalized.shape[1]
-    X_predict = features_normalized[-window:]  # 使用前 window 个数据
-    X_predict = torch.tensor(np.array(X_predict, dtype=np.float32)).reshape(1, -1, input_dim)
+    X_predict = training_features_normalized[-window:] #取出最后training_window个元素
+    X_predict = torch.tensor(np.array(X_predict, dtype=np.float32))
+    X_predict = X_predict.resize(1, window, input_dim)
+    Y_predictions = model(X_predict)
+    print(Y_predictions.detach().numpy())
 
-    Y_predictions = []
-
-    for _ in range(int(step)):
-        Y_prediction = model(X_predict)
-        Y_prediction = scaler_goal.inverse_transform(Y_prediction.detach().numpy())
-        X_predict = torch.cat((X_predict[:, 1:], torch.tensor(Y_prediction[-1, :]).view(1, 1, -1)), dim=1)
-        Y_predictions.append(Y_prediction.flatten())
-
-    Y_predictions = np.array(Y_predictions)
     print(Y_predictions)
-
 
     plt.switch_backend('Agg')
     plt.clf()
